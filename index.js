581baac7a9f5939b2584614aa548d126b0124c46
Neculai Claudiu-Andrei <134724024+SH3FT1R@users.noreply.github.com>
2023-11-14 13:33:35 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+let saveEl = document.getElementById("save-el");
+let countEl = document.getElementById("count-el");
+let count = 0;
+
+function increment() {
+  count += 1;
+  countEl.textContent = count;
+}
+
+function save() {
+  let countStr = count + " - ";
+  saveEl.textContent += countStr;
+  countEl.textContent = 0;
+  count = 0;
+}
+
+//when i use reset button i want to reset the text in the paragraph
+
+function reset() {
+  saveEl.textContent = "Previous entries: ";
+}
